67175fce4a68a93e8444a26c25700039fc7f04cd
Nukr <nukrs.w@gmail.com>
2016-01-26 01:16:26 +0800

    reduce .babelrc complexity

--- devServer.js
+++ devServer.js
@@ -1,20 +1,26 @@
 import path from 'path'
-import koa from 'koa'
+import Koa from 'koa'
 import webpack from 'webpack'
 import config from './webpack.config.dev.babel'
 import serve from 'koa-static'
+import convert from 'koa-convert'
+import Router from 'koa-router'
 
-var app = koa()
-var compiler = webpack(config)
+const router = new Router()
+const app = new Koa()
+const compiler = webpack(config)
 
-app.use(require('koa-webpack-dev-middleware')(compiler, {
+router.get('/gg', async (ctx, next) => {
+  ctx.body = 'hihi'
+})
+
+app.use(router.routes())
+app.use(convert(require('koa-webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}))
-
-app.use(require('koa-webpack-hot-middleware')(compiler))
-
-app.use(serve(path.resolve(__dirname, 'public')))
+})))
+app.use(convert(require('koa-webpack-hot-middleware')(compiler)))
+app.use(convert(serve(path.resolve(__dirname, 'public'))))
 
 app.listen(3000)
 console.log('Listening at http://localhost:3000')
